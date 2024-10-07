09a84bb381f3e2278f0bb394bf6e5324f0e53945
leodesmoulins <leo.dsm22@gmail.com>
2024-10-07 11:15:52 +0200

    first commit

--- js/login.js
+++ js/login.js
@@ -5,16 +5,13 @@ document.addEventListener('DOMContentLoaded', function() {
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
         
+        // Ici, vous pouvez ajouter votre logique de vérification
+        // Pour cet exemple, nous utiliserons des identifiants codés en dur
         if (username === 'ldsm_admin' && password === 'c24P30@2000') {
             sessionStorage.setItem('isLoggedIn', 'true');
-            window.location.href = 'home.html';
+            window.location.href = '../index.html';
         } else {
             alert('Identifiants incorrects. Veuillez réessayer.');
         }
     });
 });
-
-// Check if user is already logged in
-if (sessionStorage.getItem('isLoggedIn') === 'true') {
-    window.location.href = 'home.html';
-}
